96a5fd8d3c0673dcab0725fc041372337b2dc496
Raymond Iwobi <techlodas@gmail.com>
2025-10-21 16:57:48 +0100

    new updates

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const app = express()
 
 
 app.use(cors({  
-    origin: 'https://authentication-app-fullstack-project-2.onrender.com', //'http://localhost:3000',   
+    origin: 'https://authenticate-taupe.vercel.app', //'http://localhost:3000',   
     credentials:true,
 }))
 
--- frontend/src/components/Authentication/Dashboard.js
+++ frontend/src/components/Authentication/Dashboard.js
@@ -11,7 +11,7 @@ function Dashboard() {
 
       axios.defaults.withCredentials = true
      useEffect(() => {
-         axios.get('https://authentication-app-fullstack-project-2.onrender.com/auth/verify' ,{ withCredentials: true })
+         axios.get('https://authentication-app-fullstack-project.onrender.com/auth/verify' ,{ withCredentials: true })
        //axios.get('http://localhost:10000/auth/verify' ,{ withCredentials: true })
         .then((res) => {
             if(res.data.status){
@@ -26,7 +26,7 @@ function Dashboard() {
 
   // Step 1: Get the current user 
   useEffect(() => {
-    axios.get('https://authentication-app-fullstack-project-2.onrender.com/auth/dashboard', { withCredentials: true })
+    axios.get('https://authentication-app-fullstack-project.onrender.com/auth/dashboard', { withCredentials: true })
     //axios.get('http://localhost:10000/auth/dashboard', { withCredentials: true })
       .then(res => {
         setCurrentUser(res.data);
@@ -39,7 +39,7 @@ function Dashboard() {
 
    axios.defaults.withCredentials = true
     const handleLogout = () => {
-    axios.get('https://authentication-app-fullstack-project-2.onrender.com/auth/logout' , { withCredentials: true })
+    axios.get('https://authentication-app-fullstack-project.onrender.com/auth/logout' , { withCredentials: true })
        //axios.get('http://localhost:10000/auth/logout' , { withCredentials: true })
       .then(res => {
         if(res.data.status){
